08debb804ab1ffecca2672f7e17ee8e5be3b68d4
laplace4a <laplace47594a@gmail.com>
2024-08-06 21:39:58 +0900

    fix: folder struceter, pakage name

--- src/main/java/com/kert/Application.java
+++ src/main/java/com/kert/Application.java
@@ -1,13 +1,13 @@
-package com.kert.intro;
+package com.kert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntroApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/kert/ApplicationTests.java
+++ src/test/java/com/kert/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kert.intro;
+package com.kert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IntroApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
