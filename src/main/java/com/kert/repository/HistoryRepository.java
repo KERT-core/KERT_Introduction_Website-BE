87dad5ae30db2d3dcb7283684271fa306cdc6c1d
laplace4a <laplace47594a@gmail.com>
2024-08-10 22:48:51 +0900

    feat: Add HistoryRepository

---
+++ src/main/java/com/kert/repository/HistoryRepository.java
@@ -0,0 +1,9 @@
+package com.kert.repository;
+
+import com.kert.model.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HistoryRepository extends JpaRepository<History, Long> {
+}
