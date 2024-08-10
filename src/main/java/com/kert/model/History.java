75343a79d547b64da9eaf9c7a9e6f8931a29ba1b
laplace4a <laplace47594a@gmail.com>
2024-08-10 23:14:52 +0900

    fix: History model structure simplify

--- src/main/java/com/kert/model/History.java
+++ src/main/java/com/kert/model/History.java
@@ -6,29 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "`history`")
 public class History {
-    //제 생각은 이게 아니었는데 identifier가 없으면 안된대서 넣었어요
-    //이게 리스트형태로 나중에 나가야하는데 pubilc class List<History>로는 안되길래
-    //oneTomany manyToone으로 구성했는데 이러니까 id가 2개 들어가네요 어째야할까요
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @OneToMany(mappedBy = "history", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<HistoryList> history;
-}
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class HistoryList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -44,7 +28,4 @@ class HistoryList {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at")
     private Date updatedAt;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private History history;
 }
