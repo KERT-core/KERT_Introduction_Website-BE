ed5e7b513811cc20c4bf8fd63de36b7b3379f704
cocoquiet <cocoquiet@knu.ac.kr>
2024-08-11 13:42:07 +0900

    Feat: Add attribute(created_at, updated_at)

--- src/main/java/com/kert/model/User.java
+++ src/main/java/com/kert/model/User.java
@@ -4,8 +4,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "`user`")
 @Data
 @NoArgsConstructor
@@ -20,4 +26,11 @@ public class User {
     private String profilePicture;
     private String generation;
     private String major;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
