4c12bc0bd7016b7d5878afa6ff7752445df362d5
Nishtha lath <90383137+NishthaLath@users.noreply.github.com>
2024-08-13 19:30:44 +0900

    Updated changes in Password.java
    Used Spring Data JPA annotations to automatically manage time-related properties - createdAt and updatedAt instead of manually setting them with @PrePersist and @PreUpdate methods.

--- src/main/java/com/kert/model/Password.java
+++ src/main/java/com/kert/model/Password.java
@@ -4,13 +4,20 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "`password`")
+@EntityListeners(AuditingEntityListener.class)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Password {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,16 +28,11 @@ public class Password {
     @Column(nullable = false)
     private String hash;
 
+    @CreatedDate
+    @Column(nullable = false, updatable = false) // Not updatable once created
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
 }
