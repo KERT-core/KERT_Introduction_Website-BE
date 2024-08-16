310f79ee767fa5bc279586915ac828c021f36844
laplace4a <laplace47594a@gmail.com>
2024-08-17 02:34:00 +0900

    feat: add post model

---
+++ src/main/java/com/kert/model/Post.java
@@ -0,0 +1,45 @@
+package com.kert.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String title;
+
+    @Column(length = 100)
+    private String tag;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Long adminId;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
