bf85dda00a284888da603bbacdff5bccb3bbf183
laplace4a <laplace47594a@gmail.com>
2024-08-17 22:32:44 +0900

    feat: add PostRepository

---
+++ src/main/java/com/kert/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.kert.repository;
+
+import com.kert.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
