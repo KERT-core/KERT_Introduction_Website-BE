9cf4b72aa5cf6adfdd2e2935bbcc738644036aa8
laplace4a <laplace47594a@gmail.com>
2024-08-17 22:33:31 +0900

    feat: add PostController

---
+++ src/main/java/com/kert/controller/PostController.java
@@ -0,0 +1,52 @@
+package com.kert.controller;
+
+import com.kert.model.Post;
+import com.kert.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        Post createdPost = postService.createPost(post);
+        return ResponseEntity.ok(createdPost);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getPostsByTag(@RequestParam(required = false) String tag) {
+        List<Post> posts;
+        if (tag != null) {
+            posts = postService.getPostsByTag(tag);
+        } else {
+            posts = postService.getAllPosts();
+        }
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id) {
+        Post post = postService.getPostById(id);
+        return ResponseEntity.ok(post);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post postDetails) {
+        Post updatedPost = postService.updatePost(id, postDetails);
+        return ResponseEntity.ok(updatedPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+    }
+}
