d5f2445fe9ccfafeca1544d210d19e1cbad53764
LAPLACE4A <47558679+LAPLACE4A@users.noreply.github.com>
2024-08-18 19:01:19 +0900

    Feat/post (#11)
    * feat: add post model
    
    * fix: post model realation
    
    * fix: post model relation column
    
    * feat: add PostRepository
    
    * feat: add PostController
    
    * feat: add PostService
    
    * fix: simplify postService getPostById

---
+++ src/main/java/com/kert/model/Post.java
@@ -0,0 +1,50 @@
+package com.kert.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String title;
+
+    @Column(length = 100)
+    private String tag;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/kert/service/PostService.java
@@ -0,0 +1,48 @@
+package com.kert.service;
+
+import com.kert.model.Post;
+import com.kert.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    public List<Post> getPostsByTag(String tag) {
+        List<Post> allPosts = postRepository.findAll();
+        return allPosts.stream()
+                .filter(post -> tag.equals(post.getTag()))
+                .collect(Collectors.toList());
+    }
+
+    public Post updatePost(Long id, Post postDetails) {
+        Post post = getPostById(id);
+        post.setTitle(postDetails.getTitle());
+        post.setTag(postDetails.getTag());
+        post.setContent(postDetails.getContent());
+        return postRepository.save(post);
+    }
+
+    public void deletePost(Long id) {
+        Post post = getPostById(id);
+        postRepository.delete(post);
+    }
+}
