fb86c0a261a149473bf87cc1856d6d9aceda0d26
laplace4a <laplace47594a@gmail.com>
2024-09-06 22:20:26 +0900

    feat: add anootation enablewebsecurity

--- src/main/java/com/kert/config/SecurityConfig.java
+++ src/main/java/com/kert/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.kert.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,8 +24,8 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authz -> authz
                 .anyRequest().permitAll()
-            );
+            ).sessionManagement(session -> session
+        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));  
         return http.build();
     }
-
 }
