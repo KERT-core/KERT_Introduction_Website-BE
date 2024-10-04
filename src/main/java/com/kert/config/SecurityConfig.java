a24a0c3f451724662106292dad5b6eece32cf0fc
LAPLACE4A <laplace47594a@gmail.com>
2024-10-04 16:08:10 +0900

    fix: cors setting by pattern

--- src/main/java/com/kert/config/SecurityConfig.java
+++ src/main/java/com/kert/config/SecurityConfig.java
@@ -82,6 +82,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
