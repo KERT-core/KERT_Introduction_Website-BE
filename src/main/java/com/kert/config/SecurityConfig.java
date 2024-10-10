7b94d337be28a0bb78c9169beb4a3c6c64d8a6b9
LAPLACE4A <laplace47594a@gmail.com>
2024-10-10 15:17:03 +0900

    fix: login, get user auth err

--- src/main/java/com/kert/config/SecurityConfig.java
+++ src/main/java/com/kert/config/SecurityConfig.java
@@ -6,14 +6,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,62 +17,46 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.kert.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
-
-import java.util.HashSet;
-import java.util.Set;
 import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final AdminRepository adminRepository;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final SecurityUserService securityUserService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .authorizeHttpRequests(authz -> authz
-            .requestMatchers("/", "/auth/**", "/users/login", "/users/signup").permitAll()
-            .requestMatchers(HttpMethod.POST, "/admin/**", "/posts/**", "/histories/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.PUT, "/admin/**", "/posts/**", "/histories/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.DELETE, "/admin/**", "/posts/**", "/histories/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.GET,"/users/**").hasAnyRole("USER", "ADMIN")
-            .requestMatchers(HttpMethod.GET, "/posts", "/histories").permitAll()
-//            .requestMatchers("/h2-console/**").permitAll()
-            .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                .loginPage("/")
-                .defaultSuccessUrl("/users")
-                .permitAll()
-            )
-//            .oauth2Login(oauth2 -> oauth2
-//                .loginPage("/")
-//                .defaultSuccessUrl("/users", true)
-//                .userInfoEndpoint(userInfo -> userInfo
-//                    .userAuthoritiesMapper(grantedAuthoritiesMapper())
-//                )
-//            )
-            .sessionManagement(session -> session
-                // .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .headers(headers -> headers
-                    .frameOptions(frameOptions -> frameOptions.disable())
-            );
-            http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/users/login", "/users/signup").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/admin/**", "/posts/**", "/histories/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/admin/**", "/posts/**", "/histories/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/admin/**", "/posts/**", "/histories/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/users/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/posts", "/histories").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return securityUserService;
     }
 
     @Bean
@@ -93,47 +73,8 @@ public class SecurityConfig {
         return source;
     }
 
-    private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
-        return (authorities) -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-
-            authorities.forEach(authority -> {
-                if (authority instanceof OidcUserAuthority) {
-                    mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-                    if (isAdmin((OidcUserAuthority) authority)) {
-                        mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-                    }
-                } else if (authority instanceof OAuth2UserAuthority) {
-                    mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-                    if (isAdmin((OAuth2UserAuthority) authority)) {
-                        mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-                    }
-                } else {
-                    mappedAuthorities.add(authority);
-                }
-            });
-
-            return mappedAuthorities;
-        };
-    }
-
     @Bean
     public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
-
-    private boolean isAdmin(OidcUserAuthority oidcUserAuthority) {
-        Long userId = Long.valueOf((String) oidcUserAuthority.getAttributes().get("sub"));
-        return checkIfUserIsAdmin(userId);
-    }
-    
-    private boolean isAdmin(OAuth2UserAuthority oauth2UserAuthority) {
-        Long userId = Long.valueOf((String) oauth2UserAuthority.getAttributes().get("sub"));
-        return checkIfUserIsAdmin(userId);
-    }
-
-    private boolean checkIfUserIsAdmin(Long userId) {
-        return adminRepository.findById(userId).isPresent();
-    }
-
 }
