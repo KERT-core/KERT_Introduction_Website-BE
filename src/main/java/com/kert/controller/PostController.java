6f3148581ccdfc7157612c7b175456e7d8af4c7c
LAPLACE4A <laplace47594a@gmail.com>
2024-11-03 21:09:36 +0900

    feat: post service logic with controller fix

--- src/main/java/com/kert/controller/PostController.java
+++ src/main/java/com/kert/controller/PostController.java
@@ -3,10 +3,10 @@ package com.kert.controller;
 import com.kert.model.Post;
 import com.kert.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -16,19 +16,18 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody Post post) {
-        Post createdPost = postService.createPost(post);
+    public ResponseEntity<Post> createPost(@RequestBody Post post, HttpServletRequest request) {
+        Post createdPost = postService.createPost(post, request);
         return ResponseEntity.ok(createdPost);
     }
 
     @GetMapping
-    public ResponseEntity<List<Post>> getPostsByTag(@RequestParam(required = false, name = "tag") String tag) {
-        List<Post> posts;
-        if (tag != null) {
-            posts = postService.getPostsByTag(tag);
-        } else {
-            posts = postService.getAllPosts();
-        }
+    public ResponseEntity<Page<Post>> getPosts(
+            @RequestParam(required = false) String search,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Page<Post> posts = postService.searchPosts(search, page, size);
         return ResponseEntity.ok(posts);
     }
 
--- src/main/java/com/kert/repository/PostRepository.java
+++ src/main/java/com/kert/repository/PostRepository.java
@@ -1,12 +1,10 @@
 package com.kert.repository;
 
 import com.kert.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-  List<Post> findByTag(String Tag);
+  Page<Post> findByTitleContainingOrTagContaining(String title, String tag, Pageable pageable);
 }
